247dafcd6da857892d33d7e090f374e10a81cf58
Dileepnalluri36 <dp7730899@gmail.com>
2026-03-05 01:17:57 +0530

    Jenkins file fixes

--- src/main/java/driver/DriverFactory.java
+++ src/main/java/driver/DriverFactory.java
@@ -2,6 +2,9 @@ package driver;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DriverFactory {
 	private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
@@ -14,7 +17,12 @@ public class DriverFactory {
 			{
 			case "chrome":
 			default:
-				driver.set(new ChromeDriver());
+				WebDriverManager.chromedriver().setup();
+				ChromeOptions options = new ChromeOptions();
+				options.addArguments("--headless=new");
+				options.addArguments("--disable-gpu");
+				options.addArguments("--window-size=1920,1080");
+				driver.set(new ChromeDriver(options));
 			}
 		}
 	}
